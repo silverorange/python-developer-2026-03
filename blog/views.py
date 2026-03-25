232172869e9cc3917887ee2495e1e53bae1e0e7a
Nhat Tan Vu <onchua2006@gmail.com>
2026-03-24 20:01:35 -0700

    Load all published posts to view and click on a post to go to the post details

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from blog.models import Post
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "blog/index.html")
+    posts = (
+        Post.objects
+        .select_related("author")
+        .filter(
+            published_at__isnull=False,
+        )
+        .order_by("-published_at"))
+    return render(
+        request, 
+        "blog/index.html",
+        {
+            "posts": posts
+        })
 
 
 def welcome(request: HttpRequest) -> HttpResponse:
